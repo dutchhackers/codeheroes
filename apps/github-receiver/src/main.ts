b37bd135c764f6f43536fb7f2ef6adea7a700e5b
Michael Schilling <michael.schilling@moveagency.com>
2024-12-23 16:03:13 +0100

    chore: simplify app structure

--- apps/github-receiver/src/main.ts
+++ apps/github-receiver/src/main.ts
@@ -2,8 +2,7 @@ import { setGlobalOptions } from 'firebase-functions/v2/options';
 import { onRequest } from 'firebase-functions/v2/https';
 import { DEFAULT_REGION } from '@codeheroes/common';
 
-import { GitHubReceiverApp } from './app/app';
-
+import { GitHubReceiverApp } from './app';
 
 setGlobalOptions({ region: DEFAULT_REGION });
 
