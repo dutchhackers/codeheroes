f5e20c56a128ed91c996a0a80108ac885aa185ea
Michael Schilling <michael.schilling@moveagency.com>
2024-12-23 18:46:23 +0100

    chore: add github models

---
+++ packages/github-models/src/lib/github-models.spec.ts
@@ -0,0 +1,7 @@
+import { githubModels } from './github-models';
+
+describe('githubModels', () => {
+  it('should work', () => {
+    expect(githubModels()).toEqual('github-models');
+  });
+});
---
+++ packages/github-models/src/lib/github-models.ts
@@ -0,0 +1,3 @@
+export function githubModels(): string {
+  return 'github-models';
+}
