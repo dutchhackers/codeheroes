2ac3134e18335ae5807706df7fa7a895744ba968
Michael Schilling <michael.schilling@moveagency.com>
2025-01-12 16:09:05 +0100

    set region based on constant

--- apps/game-engine/src/main.ts
+++ apps/game-engine/src/main.ts
@@ -2,6 +2,12 @@ import * as logger from 'firebase-functions/logger';
 import { onRequest } from 'firebase-functions/v2/https';
 import { onDocumentCreated } from 'firebase-functions/v2/firestore';
 
+import { setGlobalOptions } from 'firebase-functions/v2';
+import { DEFAULT_REGION } from '@codeheroes/common';
+
+setGlobalOptions({ region: DEFAULT_REGION });
+
+
 export const healthCheck = onRequest((request, response) => {
   logger.info("Health check requested", {structuredData: true});
   response.status(200).json({
@@ -13,5 +19,4 @@ export const healthCheck = onRequest((request, response) => {
 
 export const handleEventCreation = onDocumentCreated('events/{eventId}', (event) => {
   logger.info('New event document created', { eventId: event.params.eventId });
-  throw new Error('Not Implemented');
 });
