6ab890ae363cfc403da5e10c891cc2ef3d68ae82
Michael Schilling <michael.schilling@moveagency.com>
2025-01-12 17:06:30 +0100

    add actor prop

--- libs/common/src/models/event.model.ts
+++ libs/common/src/models/event.model.ts
@@ -1,10 +1,16 @@
 import { BaseDocument, ConnectedAccountProvider } from '.';
 
+interface Actor {
+  id: string;
+  username?: string;
+}
+
 // Event Document
 export interface WebhookEvent extends BaseDocument {
   action: string; // e.g. 'github.push', 'github.pull_request.opened', 'github.pull_request.merged'
   source: ConnectedAccountProvider;
   eventId: string;
+  actor: Actor;
   eventTimestamp: string; // ISO string
   details: Record<string, unknown>;
   processed: boolean;
