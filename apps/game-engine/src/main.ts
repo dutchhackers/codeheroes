2d079ff28c912e4bd4b36d1c0f51b616fcda10f8
Michael Schilling <michael.schilling@moveagency.com>
2025-01-12 16:05:38 +0100

    chore: add new trigger on events

--- apps/game-engine/src/main.ts
+++ apps/game-engine/src/main.ts
@@ -1,5 +1,6 @@
 import * as logger from 'firebase-functions/logger';
 import { onRequest } from 'firebase-functions/v2/https';
+import { onDocumentCreated } from 'firebase-functions/v2/firestore';
 
 export const healthCheck = onRequest((request, response) => {
   logger.info("Health check requested", {structuredData: true});
@@ -8,4 +9,9 @@ export const healthCheck = onRequest((request, response) => {
     timestamp: new Date().toISOString(),
     service: 'game-engine'
   });
+});
+
+export const handleEventCreation = onDocumentCreated('events/{eventId}', (event) => {
+  logger.info('New event document created', { eventId: event.params.eventId });
+  throw new Error('Not Implemented');
 });
