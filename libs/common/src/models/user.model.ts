893ebb0766f45aa9c46feb63a06dbab2b492162c
Michael Schilling <michael.schilling@moveagency.com>
2025-01-14 21:07:23 +0100

    simplify user model

--- libs/common/src/models/user.model.ts
+++ libs/common/src/models/user.model.ts
@@ -1,4 +1,5 @@
 import { BaseDocument } from './common.model';
+import { EventSource } from './event.model';
 
 export interface CreateUserInput {
   uid?: string;
@@ -16,3 +17,40 @@ export interface User extends BaseDocument {
   active: boolean;
   uid?: string;
 }
+
+
+interface BaseActivityData {
+  type: string;
+}
+
+export interface PushActivityData extends BaseActivityData {
+  type: 'push';
+  commitCount: number;
+  branch: string;
+}
+
+export interface PullRequestActivityData extends BaseActivityData {
+  type: 'pull_request';
+  prNumber: number;
+  title: string;
+  merged: boolean;
+}
+
+export interface IssueActivityData extends BaseActivityData {
+  type: 'issue';
+  issueNumber: number;
+  title: string;
+}
+
+export type ActivityData = PushActivityData | PullRequestActivityData | IssueActivityData;
+
+export interface UserActivity extends BaseDocument {
+  action: string;
+  eventId: string; // In the future this might become an optional field
+  userId: string;
+  eventSource: EventSource;
+  userFacingDescription: string;
+  data?: ActivityData;
+}
+
+export type CreateActivityInput = Omit<UserActivity, 'id' | 'createdAt' | 'updatedAt'>;
