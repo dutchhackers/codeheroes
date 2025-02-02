02343ecd079d456be70147c955fa51339ebfd1a1
Michael Schilling <michael.schilling@moveagency.com>
2025-02-02 20:34:36 +0100

    disconnect migration usage (api)

--- apps/api/src/controllers/characters-controller.ts
+++ apps/api/src/controllers/characters-controller.ts
@@ -1,20 +1,19 @@
-import { CharacterService } from '@codeheroes/migration';
 import * as express from 'express';
 
 const router = express.Router();
 
 router.get('/:id?', async (req, res) => {
-  const characterService = new CharacterService();
-  const id = req.params.id;
-  if (id) {
-    res.json(await characterService.getCharacter(id));
-  } else {
-    const characters = await characterService.getCharacters();
-    for (const item of characters as any[]) {
-      delete item.teamRef;
-    }
-    res.json(characters);
-  }
+  // const characterService = new CharacterService();
+  // const id = req.params.id;
+  // if (id) {
+  //   res.json(await characterService.getCharacter(id));
+  // } else {
+  //   const characters = await characterService.getCharacters();
+  //   for (const item of characters as any[]) {
+  //     delete item.teamRef;
+  //   }
+  //   res.json(characters);
+  // }
 });
 
 export { router as CharactersController };
