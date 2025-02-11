f86c932a156fe78ee58a139f4a4de1d0417e04b2
Michael Schilling <michael.schilling@moveagency.com>
2025-02-11 12:30:43 +0100

    tweak env file creation

--- scripts/setup-firebase.js
+++ scripts/setup-firebase.js
@@ -6,7 +6,7 @@ const firebasercTemplate = fs.readFileSync('.firebaserc.template', 'utf8');
 const firebasercConfig = firebasercTemplate.replace('${FIREBASE_PROJECT_ID}', process.env.FIREBASE_PROJECT_ID);
 fs.writeFileSync('.firebaserc', firebasercConfig);
 
-// Generate environment.prod.ts file
+// Generate environment.{local,prod}.ts file
 const envTemplate = fs.readFileSync('apps/web/src/environments/environment.ts.template', 'utf8');
 const envConfig = envTemplate
   .replace('${FIREBASE_API_KEY}', process.env.FIREBASE_API_KEY)
@@ -15,4 +15,7 @@ const envConfig = envTemplate
   .replace('${FIREBASE_STORAGE_BUCKET}', process.env.FIREBASE_STORAGE_BUCKET)
   .replace('${FIREBASE_MESSAGING_SENDER_ID}', process.env.FIREBASE_MESSAGING_SENDER_ID)
   .replace('${FIREBASE_APP_ID}', process.env.FIREBASE_APP_ID);
+
+// Generate both environment files
+fs.writeFileSync('apps/web/src/environments/environment.local.ts', envConfig);
 fs.writeFileSync('apps/web/src/environments/environment.prod.ts', envConfig);
