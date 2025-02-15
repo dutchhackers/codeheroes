a5b04be953f76f28fb1653d9721cc29a940bc438
Michael Schilling <michael.schilling@moveagency.com>
2025-02-15 15:27:36 +0100

    initialize app in main

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,12 +1,13 @@
 import 'reflect-metadata';
+import admin from 'firebase-admin';
 import { onRequest } from 'firebase-functions/v2/https';
 import { setGlobalOptions } from 'firebase-functions/v2';
 import { DEFAULT_REGION } from '@codeheroes/common';
 
 setGlobalOptions({ region: DEFAULT_REGION });
 
-// import { connectFirestore } from '@codeheroes/common';
-// connectFirestore();
+// Calling initializeApp directly; to be improved in the future by using initializeApp from common
+admin.initializeApp();
 
 export * from './triggers/on-before-user-created.trigger';
 export * from './triggers/on-before-user-sign-in.trigger';
