7c7ba06ee7acc9bab38654483a4d50c05b9d0047
Michael Schilling <michael.schilling@moveagency.com>
2025-03-02 14:16:42 +0100

    simplify and cleanup integration lib

--- libs/server/integrations/src/lib/index.ts
+++ libs/server/integrations/src/lib/index.ts
@@ -1,5 +1,4 @@
-// export * from './actions';
 export * from './providers/interfaces/provider.interface';
 export * from './providers/provider.factory';
 export * from './providers/github/github.adapter';
-export * from './actions/game-action.service';
+export * from './services/game-action.service';
